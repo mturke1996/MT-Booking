289ef640600843c255c5fc643fef4a4ce1b5deed
Rouba7 <135597746+Rouba7@users.noreply.github.com>
2024-08-19 14:32:20 +0200

    footer

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./Hero";
 import Statistic from "./Statistic";
 import Apartment from "./Apartment";
+import Footer from "./Footer";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Statistic />
       <Apartment />
+      <Footer/>
     </div>
   );
 }
