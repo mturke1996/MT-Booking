cc1e671efd99807aa490d3962d7a44b7c7d179d5
Mohamed <Mohturke96@gmail.com>
2024-08-20 14:49:04 +0200

    hally

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,76 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
 
 function Hero() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [apartments, setApartments] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (searchQuery.trim() !== "") {
-      fetchApartments();
-    }
-  }, [searchQuery]);
-
-  const fetchApartments = () => {
-    setIsLoading(true);
-    setError(null);
-
-    fetch(`http://localhost:5000/api/apartments`)
-      .then((response) => response.json())
-      .then((data) => {
-        const filteredData = data.filter((apartment) =>
-          apartment.Adresse.toLowerCase().includes(searchQuery.toLowerCase())
-        );
-        setApartments(filteredData);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setError("Error fetching apartments");
-        setIsLoading(false);
-      });
-  };
-
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    fetchApartments();
-
-    // التنقل إلى صفحة النتائج مع تمرير الشقق المصفاة
-    navigate("/Searchitem", {
-      state: { results: apartments },
-    });
+  const handleGetStartedClick = () => {
+    // التنقل إلى صفحة البحث أو أي صفحة أخرى
+    navigate("/Searchitem");
   };
 
   return (
-    <div>
-      <header className="hero">
-        <div className="hero-content">
-          <div className="search-bar">
-            <div>
-              <h1>Good Morning!</h1>
-              <p>Explore beautiful places in the world with MT Booking</p>
-            </div>
-            <div>
-              <input
-                type="text"
-                placeholder="Enter Address"
-                value={searchQuery}
-                onChange={handleSearchChange}
-              />
-              <button onClick={handleSearchSubmit}>Search</button>
-            </div>
-          </div>
-        </div>
-      </header>
-    </div>
+    <header className="hero">
+      <div className="hero-content">
+        <h1>Good Morning!</h1>
+        <p>Explore beautiful places in the world with MT Booking</p>
+        <button onClick={handleGetStartedClick}>Get Started</button>
+      </div>
+    </header>
   );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import "../App.css";
 
 function Navbar({ user, onLogout }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -10,21 +17,11 @@ function Navbar({ user, onLogout }) {
       </div>
 
       <ul className="navbar-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/Weather">Weather</Link>
-        </li>
-        <li>
-          <Link to="#">Blog</Link>
-        </li>
-        <li>
-          <Link to="#">News</Link>
-        </li>
-        <li>
-          <Link to="#">Contact</Link>
-        </li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Weather">Weather</Link></li>
+        <li><Link to="#">Blog</Link></li>
+        <li><Link to="#">News</Link></li>
+        <li><Link to="#">Contact</Link></li>
       </ul>
 
       <div className="user-info">
@@ -33,40 +30,22 @@ function Navbar({ user, onLogout }) {
             <span className="welcome-message">
               <i className="fa-solid fa-user fa-beat-fade"></i> Hi, {user.username}
             </span>
-            <div className="user-menu">
+            <div className="user-menu" onClick={toggleMenu}>
               <span className="icon">
                 <span></span>
                 <span></span>
                 <span></span>
               </span>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link to="/profile" className="profile-link">
-                    My Profile
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/myBooking" className="profile-link">
-                    My Booking
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/addApartment" className="profile-link">
-                    Add New Apartment
-                  </Link>
-                </li>
-                <li>
-                  <button onClick={onLogout} className="logout-button">
-                    Logout
-                  </button>
-                </li>
+              <ul className={`dropdown-menu ${isMenuOpen ? 'visible' : 'hidden'}`}>
+                <li><Link to="/profile" className="profile-link">My Profile</Link></li>
+                <li><Link to="/myBooking" className="profile-link">My Booking</Link></li>
+                <li><Link to="/addApartment" className="profile-link">Add New Apartment</Link></li>
+                <li><button onClick={onLogout} className="logout-button">Logout</button></li>
               </ul>
             </div>
           </>
         ) : (
-          <Link to="/login" className="login-button">
-            Login
-          </Link>
+          <Link to="/login" className="login-button">Login</Link>
         )}
       </div>
     </nav>
