19d1e92b610a5726ecc9ea0625bc7bcb4a9f51e5
Mohamed <Mohturke96@gmail.com>
2024-08-27 12:57:03 +0200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -94,7 +94,7 @@ function App() {
             }
           />
           <Route
-            path="//Payment"
+            path="/Payment"
             element={
               user ? <CheckoutForm user={user} /> : <Navigate to="/login" />
             }
